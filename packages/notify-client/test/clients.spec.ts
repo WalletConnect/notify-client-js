60867d6ab77062c5b7736c06fcfb98ebaba5060e
Celine Sarafa <celinesarafa@gmail.com>
2023-11-22 13:37:08 +0100

    Update packages/notify-client/test/clients.spec.ts
    Co-authored-by: Ben Kremer <ben@walletconnect.com>

--- packages/notify-client/test/clients.spec.ts
+++ packages/notify-client/test/clients.spec.ts
@@ -884,7 +884,7 @@ describe("Notify", () => {
 
         const encoded = await wallet.core.crypto.encode(testSub.topic, {
           ...message,
-          // Set different JSONRPC ID
+          // Set different JSONRPC ID to avoid the relayer deduping the message based on JSON-RPC ID
           // Deduping should be done based on notify message ID, not
           // JSONRPC payload id
           id: Date.now(),
