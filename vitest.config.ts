09fdc9de99b8fc286cf2b51abf20dc886ceb292f
Celine Sarafa <celinesarafa@gmail.com>
2024-01-11 14:09:58 +0100

    chore: format vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,14 +5,11 @@ import testEnv from "./testEnv.json";
 export default defineConfig({
   test: {
 onStackTrace(error: Error, { file }: ParsedStack): boolean | void {
-      // If we've encountered a ReferenceError, show the whole stack.
-      if (error.name === 'ReferenceError')
-        return
-
-      // Reject all frames from third party libraries.
+      // Reject all traces from third parties
       if (file.includes('node_modules'))
         return false
-    },    env: {
+    },
+    env: {
       ...testEnv,
     },
     testTimeout: 60_000,
