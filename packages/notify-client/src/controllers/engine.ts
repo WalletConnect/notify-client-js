445fe4f5a7250dca0ee4dc79889071c0b904a249
Celine Sarafa <celinesarafa@gmail.com>
2024-01-11 10:47:58 +0100

    chore: get longer stack traces

--- packages/notify-client/src/controllers/engine.ts
+++ packages/notify-client/src/controllers/engine.ts
@@ -1003,6 +1003,7 @@ export class NotifyEngine extends INotifyEngine {
         // Even if there was an error, loading is technically complete
         this.finishedInitialLoad = true;
 
+	console.log("Watch already stopped >>>> ")
         this.client.logger.error({
           event: "onNotifyWatchSubscriptionsResponse",
           topic,
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,19 @@ import testEnv from "./testEnv.json";
 
 export default defineConfig({
   test: {
-    env: {
+onStackTrace(error: Error, { file }: ParsedStack): boolean | void {
+      // If we've encountered a ReferenceError, show the whole stack.
+      if (error.name === 'ReferenceError')
+        return
+
+      // Reject all frames from third party libraries.
+      if (file.includes('node_modules'))
+        return false
+    },    env: {
       ...testEnv,
     },
     testTimeout: 60_000,
     hookTimeout: 10_000,
   }, 
+  
 });
