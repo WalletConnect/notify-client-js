8a869c5e61309e14c30e6237bd1376b797f22733
Celine Sarafa <celinesarafa@gmail.com>
2024-01-12 11:16:02 +0100

    remove onStackTrace

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,8 @@
-import { ParsedStack } from "vitest";
 import { defineConfig } from "vitest/config";
 import testEnv from "./testEnv.json";
 
 export default defineConfig({
   test: {
-onStackTrace(error: Error, { file }: ParsedStack): boolean | void {
-      // Reject all traces from third parties
-      if (file.includes('node_modules'))
-        return false
-    },
     env: {
       ...testEnv,
     },
