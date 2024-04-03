617f9716131f45e338d3d77527f796c9bc4eadfb
Celine Sarafa <celinesarafa@gmail.com>
2024-04-03 12:36:33 +0200

    Update packages/notify-client/scripts/update_sdk_versions.sh

--- packages/notify-client/scripts/update_sdk_versions.sh
+++ packages/notify-client/scripts/update_sdk_versions.sh
@@ -2,7 +2,6 @@
 
 notify_ver=$(grep "version" package.json | head -n1 | awk -F'"' '{print $4;}')
 
-
 version_line="export const NOTIFY_SDK_VERSION = "\"$notify_ver\"";"
 
 echo "$version_line" > src/constants/sdk_version.ts
